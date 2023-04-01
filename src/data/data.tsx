254f9222ff6a519a6f28de5190d262fba529d11d
camille-sophiie <83288296+camille-sophiie@users.noreply.github.com>
2023-04-01 14:27:05 +0200

    Update data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -90,11 +90,11 @@ export const heroData: Hero = {
       primary: true,
       Icon: DownloadIcon,
     },
-    {
+/*     {
       href: `#${SectionId.Contact}`,
       text: 'Contact',
       primary: false,
-    },
+    }, */
   ],
 };
 
@@ -103,16 +103,14 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `I am passionate about cloud architectures and cats. Let's have coffee!`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: OfficeBuildingIcon},
+    {label: 'Location', text: 'Zurich,  Switzerland', Icon: MapIcon},
+    {label: 'Age', text: '28', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Switzerland/ Belgium / France', Icon: FlagIcon},
+    {label: 'Interests', text: 'Books, Food, Pilates, Cats', Icon: SparklesIcon},
+    {label: 'Study', text: 'Lucerne University of Applied Sciences', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Working Student at AWS', Icon: OfficeBuildingIcon},
   ],
 };
 
@@ -312,11 +310,11 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      name: 'My mom',
+      text: 'Camille is fun to be around but usually d',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
-    {
+    /* {
       name: 'Jane Doe',
       text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
@@ -325,7 +323,7 @@ export const testimonial: TestimonialSection = {
       name: 'Someone else',
       text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
-    },
+    }, */
   ],
 };
 
@@ -339,34 +337,35 @@ export const contact: ContactSection = {
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'cnigon@hotmail.com',
+      href: 'mailto:cnigon@hotmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Zurich, Switzerland',
+      href: 'https://goo.gl/maps/eQfucHPdrCe42HwL7',
     },
-    {
+/*     {
       type: ContactType.Instagram,
       text: '@tbakerx',
       href: 'https://www.instagram.com/tbakerx/',
-    },
-    {
+    }, */
+    /* {
       type: ContactType.Github,
       text: 'tbakerx',
       href: 'https://github.com/tbakerx',
-    },
+    } */,
   ],
-};
+}; 
+
 
 /**
  * Social items
  */
 export const socialLinks: Social[] = [
-   {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+/*    {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
+  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'}, */
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/camille-nigon/'},
+/*   {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'}, */
 ];
